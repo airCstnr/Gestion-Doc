3040f220204570f46c03fbe09f286d50f6d7f9a0
airCstnr <raphael.castanier@free.fr>
2017-02-07 14:58:59 +0100

    NON FONCTIONNEL Ajout des attributs de Oeuvre

--- src/projetbiblio/Oeuvre.java
+++ src/projetbiblio/Oeuvre.java
@@ -3,5 +3,16 @@ package projetbiblio;
 
 
 public class Oeuvre {
+    
+
+    // Attributs
+    private String numISBN; // {unique}
+    private String titre;
+    private String nomEditeur;
+    private int dateParution; // type date Date (voir type calendrier grégorien)
+    private String nomAuteur;
+    private EnumPublic public; // EnumPublic
+    private int dernierExemplaire;
+    private int nombreExemplaire;
 
 }
