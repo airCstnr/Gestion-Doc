0750eafb0313ad0555f5c9265ce69b445b87ee3d
airCstnr <raphael.castanier@free.fr>
2017-02-08 08:45:30 +0100

    Ajout des getteurs, des affichages et de la liste des étudiants

--- src/projetbiblio/Oeuvre.java
+++ src/projetbiblio/Oeuvre.java
@@ -14,6 +14,65 @@ public class Oeuvre {
     }; // EnumPublic
     
     private int dernierExemplaire;
-    private int nombreExemplaire;
+    private int nombreExemplaires;
+    private HashMap<int, Exemplaire> exemplaires;
+    
+    
+   // constructeur
+    public Oeuvre(String numISBN, String titre, String nomEditeur, int dateParution, String nomAuteur) {
+        this.numISBN = numISBN;
+        this.titre = titre;
+        this.nomEditeur = nomEditeur;
+        this.dateParution = dateParution;
+        this.nomAuteur = nomAuteur;
+        this.dernierExemplaire = 1; // cet atttribut sera appelé quand on crée un nouvel exemplaire
+        this.nombreExemplaires = 0;
+        this.exemplaires = new HashMap<int, Exemplaire>();
+    }
+
+    public String getNumISBN() {
+        return numISBN;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public String getNomEditeur() {
+        return nomEditeur;
+    }
+
+    public int getDateParution() {
+        return dateParution;
+    }
+
+    public String getNomAuteur() {
+        return nomAuteur;
+    }
+
+    public int getNombreExemplaires() {
+        return nombreExemplaires;
+    }
+    
+    
+    /**
+     * Affiche le titre et l'ISBN de l'oeuvre
+     */
+    public void afficherReduit() {
+        EntreesSorties.afficherMessage("--- Titre : "+ this.getTitre() + " ---");
+        EntreesSorties.afficherMessage("N°ISBN : "+ this.getNumISBN());
+    }
+    
+    public void afficherDetails() {
+        EntreesSorties.afficherMessage("--- Auteur : "+ this.getNomAuteur() + " ---");
+        EntreesSorties.afficherMessage("--- Editeur : "+ this.getNomEditeur() + " ---");
+        EntreesSorties.afficherMessage("Date de Parution : "+ this.getDateParution());
+    }
+    
+    public void afficherExemplaire() {
+        for(Exemplaire e : exemplaires){
+            e.afficher();
+        }
+    }
 
 }
