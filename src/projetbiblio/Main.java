35e79002b36ace934c64f03dde94165f33801942
lamoira <maximedemartin@hotmail.fr>
2017-02-17 14:50:26 +0100

    Ajout du menu de gestion des fichiers

--- src/projetbiblio/Main.java
+++ src/projetbiblio/Main.java
@@ -2,6 +2,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.File;
 
 
 public class Main {
@@ -10,7 +11,45 @@ public class Main {
 
 		Bibliotheque bibliotheque = new Bibliotheque();
 		
-		String nomFich="save1.ser";
+                
+                Integer menufich,vidange,brouillon;
+                String nomFich="default.ser";
+                vidange=brouillon=0;
+                
+                do {
+                    EntreesSorties.afficherMessage(" ========================================================");
+                    EntreesSorties.afficherMessage("|            Selection Du fichier test:                  |");
+                    EntreesSorties.afficherMessage("| Fichier Vide :    1                                    |");
+                    EntreesSorties.afficherMessage("| Fichier Brouillon :   2                                |");
+                    EntreesSorties.afficherMessage("| Fichier de test(Ne pas le modifer) :     3             |");
+                    EntreesSorties.afficherMessage("| Quitter :         0                                    |");
+                    EntreesSorties.afficherMessage(" ========================================================");
+                    menufich = EntreesSorties.lireEntier();
+
+                    switch (menufich) {
+                        case 1: {
+                            nomFich="save.ser";
+                            vidange=1;
+                            break;
+                        }
+                        case 2: {
+                            nomFich="brouillon.ser";
+                            brouillon=1;
+                            break;
+                        }
+                        case 3: {
+                            nomFich="fichierTest.ser";
+                            break;
+                        }
+                        default: {
+                            break;
+                        }
+                    }
+                } while (menufich != 0 && menufich != 2 && menufich != 1 && menufich != 3);
+                
+                
+                
+		//String nomFich="backup.ser";
 		
 		/*---------------------------------------------------------------------------------------------
 		 * Récupération des serialisation précédentes dans le fichier du nom de "save.ser".
@@ -38,22 +77,118 @@ public class Main {
 		 * Sériaisation dans le fichier du nom de "save.ser".
 		 * L'utilisateur est informé de la réussite ou non de la sauvegarde des données.
 		*/
-		try {
-			FileOutputStream f = new FileOutputStream(nomFich);
-			ObjectOutputStream out = new ObjectOutputStream(f);
+                
+                if (vidange == 1)
+                {
+                    EntreesSorties.afficherMessage(" *** Start :  Merci d'avoir testé A bientôt !");
+                    EntreesSorties.afficherMessage(" *** Start :  Réinitialisation du Fichier " + nomFich);
+                }
+                else{
+                    if (brouillon == 1)
+                    {
+                        Integer menuBrouillon,zero;
+                        zero=0;
+                        
+                        do {
+                            EntreesSorties.afficherMessage(" ========================================================");
+                            EntreesSorties.afficherMessage("|            Reinitialisation du brouillon:              |");
+                            EntreesSorties.afficherMessage("| Remise à zero :    1                                   |");
+                            EntreesSorties.afficherMessage("| Sauvegarde des modifications:   2                      |");
+                            EntreesSorties.afficherMessage("| Quitter :         0                                    |");
+                            EntreesSorties.afficherMessage(" ========================================================");
+                            menuBrouillon = EntreesSorties.lireEntier();
 
-			out.writeObject(bibliotheque);
+                            switch (menuBrouillon) {
+                                case 1: {
+                                    zero=1;
+                                    break;
+                                }
+                                case 2: {
+                                    zero=2;
+                                    break;
+                                }
+                                default: {
+                                    break;
+                                }
+                            }
+                        } while (menufich != 0 && menufich != 2 && menufich != 1);
+                        
+                        /* Suppression du fichier brouillon*/
+                        
+                        if (zero == 1)
+                        {
+                            new File("./brouillon.ser").delete();                            
+                        }
+                        
+                        /* Sauvegarde du fichier brouillon*/
+                        
+                        /* Revenir sur la partie apres car bug a verfif */
+                        
+                        if (zero == 2)
+                        {
+                            try {
+                                FileOutputStream f = new FileOutputStream(nomFich);
+                                ObjectOutputStream out = new ObjectOutputStream(f);
 
-			EntreesSorties.afficherMessage(" $$$ Sauvegarde dans le fichier " + nomFich + " realisee");
+                                out.writeObject(bibliotheque);
+
+                                EntreesSorties.afficherMessage(" $$$ Sauvegarde dans le fichier " + nomFich + " realisee");
 			
-			out.close();
-			f.close();
-		} catch (Exception e) {
-			EntreesSorties.afficherMessage(" *** Start :Pbs de Sauvegarde dans le fichier " + nomFich);
-		}
-	}
-	
+                                out.close();
+                                f.close();
+                            } catch (Exception e) {
+                                EntreesSorties.afficherMessage(" *** Start :Pbs de Sauvegarde dans le fichier " + nomFich);
+                            }
+                        }
+                        
+                        /* Sauvegarde en cas de soucis avec brouillon*/
+                        
+                        else
+                        {
+                            try {
+                                FileOutputStream f = new FileOutputStream(nomFich);
+                                ObjectOutputStream out = new ObjectOutputStream(f);
+
+                                out.writeObject(bibliotheque);
+
+                                EntreesSorties.afficherMessage(" $$$ Sauvegarde dans le fichier " + nomFich + " realisee");
+			
+                                out.close();
+                                f.close();
+                            } catch (Exception e) {
+                                EntreesSorties.afficherMessage(" *** Start :Pbs de Sauvegarde dans le fichier " + nomFich);
+                            }
+                        }
+                    }
+                    
+                    /* Sauvegarde POur le cas ou on utilise le fichier : fichierTest*/
+                    
+                    else
+                    { 
+                        try {
+                                FileOutputStream f = new FileOutputStream(nomFich);
+                                ObjectOutputStream out = new ObjectOutputStream(f);
+
+                                out.writeObject(bibliotheque);
+
+                                EntreesSorties.afficherMessage(" $$$ Sauvegarde dans le fichier " + nomFich + " realisee");
+			
+                                out.close();
+                                f.close();
+                            } catch (Exception e) {
+                                EntreesSorties.afficherMessage(" *** Start :Pbs de Sauvegarde dans le fichier " + nomFich);
+                            }
+                    }
+                       
+                        
+                        
+                }
+            }
+                
+
 	public static void main(String args[]){
 	new Main();
 	}
 }
+
+ 
