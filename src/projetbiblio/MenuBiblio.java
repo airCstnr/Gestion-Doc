b10c7cfe151b66d2e6772d4d6594dd1001445fef
airCstnr <raphael.castanier@free.fr>
2017-02-17 10:47:07 +0100

    Ajout de l'entrée consulter liste ouvrages dans le MenuBiblio

--- src/projetbiblio/MenuBiblio.java
+++ src/projetbiblio/MenuBiblio.java
@@ -83,8 +83,9 @@ public class MenuBiblio {
             EntreesSorties.afficherMessage("|                   Menu Oeuvre                          |");
             EntreesSorties.afficherMessage("| Nouvel Ouvrage :                   1                   |");
             EntreesSorties.afficherMessage("| Consulter Ouvrage :                2                   |");
-            EntreesSorties.afficherMessage("| Nouvel Exemplaires :               3                   |");
-            EntreesSorties.afficherMessage("| Consulter Exemplaires Ouvrage :    4                   |");
+            EntreesSorties.afficherMessage("| Consulter Liste Ouvrages :         3                   |");
+            EntreesSorties.afficherMessage("| Nouvel Exemplaires :               4                   |");
+            EntreesSorties.afficherMessage("| Consulter Exemplaires Ouvrage :    5                   |");
             EntreesSorties.afficherMessage("| Retour Menu Principal :            0                   |");
             EntreesSorties.afficherMessage(" ========================================================");
             menuOeuvre = EntreesSorties.lireEntier();
@@ -100,10 +101,14 @@ public class MenuBiblio {
                     break;
                 }
                 case 3: {
-                    // nouvelExemplaire();
+                    // consulterListeOuvrages();
                     break;
                 }
                 case 4: {
+                    // nouvelExemplaire();
+                    break;
+                }
+                case 5: {
                     // consulterExemplairesOuvrage();
                     break;
                 }
